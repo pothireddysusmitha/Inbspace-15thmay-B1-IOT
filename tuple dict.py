bf8c0650df740b61a7e78b1e88d678b8277ae3bd
pothireddysusmitha <84527487+pothireddysusmitha@users.noreply.github.com>
2021-06-28 18:40:39 +0530

    Add files via upload

---
+++ tuple dict.py
@@ -0,0 +1,9 @@
+a = int(input("enter data for a:"))
+b = int(input("enter data for b:"))
+dictionary = {a,b}
+print("dictionary:"dictionary)
+Tuple =[a]
+Tuple.append(b)
+print("Tuple:"Tuple)
+List = (a,b)
+print("List:"List)
